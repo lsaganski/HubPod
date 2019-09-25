c030f7772d71d99724a328c058648a0294565f85
Leonardo <>
2019-09-25 14:56:21 -0300

    criado como pod

---
+++ HubPod/HubCalendar.swift
@@ -0,0 +1,15 @@
+//
+//  HubCalendar.swift
+//  HubPod
+//
+//  Created by Leonardo Saganski on 25/09/19.
+//  Copyright © 2019 ItauBBA. All rights reserved.
+//
+
+import Foundation
+
+public final class HubCalendar {
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
